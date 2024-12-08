5c31e1dbbe2f1988676c921fcaa5bb18c6e5e512
Thejandeera <thejan1010@gmail.com>
2024-12-08 23:19:39 +0530

    setuped routes (31:25)

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+      _id: "aaaaa",
+      name: "Women Round Neck Cotton Top",
+      description: "A lightweight, usually knitted, pullover shirt, close-fitting and with",
+      price: 100,
+      image: ["/p_img1.jpg"],
+      category: "Women",
+      subCategory: "Topwear",
+      sizes: ["S", "M", "L"],
+      date: 1716634345448,
+      bestseller: true
+    },
+    {
+        _id: "aaaaa",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with",
+        price: 100,
+        images: ["/p_img1.jpg", "/p_img2.jpg", "/p_img3.jpg"], // Array of image paths
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+      }
+];
+  
